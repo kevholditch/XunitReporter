3e1b22491fcdb3dcffd8a1ca2b196f059d2e3538
Kevin Holditch <kevholditch@hotmail.com>
2017-01-27 21:30:48 +0000

    removed console writeline and readkey

--- src/XunitReporter/Program.cs
+++ src/XunitReporter/Program.cs
@@ -15,7 +15,6 @@ namespace XunitReporter
 
             var reporterArgs = Args.Parse<ReporterArgs>(args);
 
-            
             Engine.Razor.Compile(AssemblyResource.InThisAssembly("TestView.cshtml").GetText(), "testTemplate", typeof(List<TestAssemblyModel>));
 
             var model = TestPageModelBuilder.Create()
@@ -27,11 +26,6 @@ namespace XunitReporter
             Engine.Razor.Run("testTemplate", output, typeof(List<TestAssemblyModel>), model);
 
             File.WriteAllText(reporterArgs.Html, output.ToString());
-
-            Console.WriteLine(output.ToString());
-
-            Console.ReadKey();
-
         }
     }
 }
